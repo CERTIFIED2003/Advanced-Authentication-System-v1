d769577eb730b58abf4d34d2656299122cb16a4d
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-04-09 10:51:55 +0530

    Fixing CORS

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     });
 }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Set-Cookie: cross-site-cookie=whatever; SameSite=None; Secure");
+    next();
+});
+
 const corsOptions = {
     origin: process.env.FRONTEND_URL,
     credentials: true,
